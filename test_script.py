8555deee8d1da2344bf95e3bf15d13b2ef559a09
Rémy Torro <50580806+remyeltorro@users.noreply.github.com>
2023-07-17 21:33:48 +0200

    Delete test_script.py

--- test_script.py
+++
@@ -1,3 +0,0 @@
-from celldetective.segmentation import segment_at_position
-
-segment_at_position("/home/limozin/Desktop/hide/MinimumJan/W4/400/", 'target', 'MCF7_w_primary_NK_BF_PI_Hoechst', use_gpu=True, view_on_napari=True)

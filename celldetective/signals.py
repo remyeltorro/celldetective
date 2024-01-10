82b79a95e9ae2907a887b2066b030b2cfa4810aa
Rémy Torro <remy.torro@etu.univ-amu.fr>
2024-01-10 12:35:08 +0100

    switch back to best model

--- celldetective/signals.py
+++ celldetective/signals.py
@@ -247,7 +247,7 @@ def analyze_signals_at_position(pos, model, mode, use_gpu=True):
 class SignalDetectionModel(object):
 	
 	def __init__(self, path=None, pretrained=None, channel_option=["live_nuclei_channel"], model_signal_length=128, n_channels=1, 
-				n_conv=3, n_classes=3, dense_collection=512, dropout_rate=0.1, label=''):
+				n_conv=1, n_classes=3, dense_collection=512, dropout_rate=0.1, label=''):
 		
 		self.prep_gpu()
 
@@ -326,8 +326,8 @@ class SignalDetectionModel(object):
 
 		"""
 
-		self.model_class = MultiscaleResNetModel(n_channels=self.n_channels,
-									#n_blocks=self.n_conv,
+		self.model_class = ResNetModelCurrent(n_channels=self.n_channels,
+									n_blocks=self.n_conv,
 									n_classes = self.n_classes,
 									dense_collection=self.dense_collection,
 									dropout_rate=self.dropout_rate, 
@@ -335,8 +335,8 @@ class SignalDetectionModel(object):
 									model_signal_length = self.model_signal_length
 									)
 
-		self.model_reg = MultiscaleResNetModel(n_channels=self.n_channels,
-									#n_blocks=self.n_conv,
+		self.model_reg = ResNetModelCurrent(n_channels=self.n_channels,
+									n_blocks=self.n_conv,
 									n_classes = self.n_classes,
 									dense_collection=self.dense_collection,
 									dropout_rate=self.dropout_rate, 
@@ -1654,119 +1654,40 @@ def MultiscaleResNetModel(n_channels, n_classes = 3, dropout_rate=0, dense_colle
 
 	return model
 
-# if header=="classifier":
-# 	final_activation = "softmax"
-# 	neurons_final = n_classes
-# elif header=="regressor":
-# 	final_activation = "linear"
-# 	neurons_final = 1
-# else:
-# 	return None
+def ResNetModelCurrent(n_channels, n_slices, depth=2, use_pooling=True, n_classes = 3, dropout_rate=0.1, dense_collection=512,
+				 	   header="classifier", model_signal_length = 128):
+	
+	if header=="classifier":
+		final_activation = "softmax"
+		neurons_final = n_classes
+	elif header=="regressor":
+		final_activation = "linear"
+		neurons_final = 1
+	else:
+		return None
 
-# inputs = Input(shape=(model_signal_length,n_channels,))
-# x2 = Conv1D(64, kernel_size=16, strides=2, padding="same")(inputs)
-# x2 = BatchNormalization()(x2)
-# x2 = MaxPooling1D()(x2)
+	n_slices = 2
+	inputs = Input(shape=(model_signal_length,n_channels,))
+	x2 = Conv1D(64, kernel_size=1,strides=1,padding='same')(inputs)
 
-# n_filters = 64
-# for k in range(depth):
-# 	for i in range(n_blocks):
-# 			x2 = residual_block1D(x2,64)
-# 	n_filters *= 2
-# 	if use_pooling and k!=(depth-1):
-# 		x2 = MaxPooling1D()(x2)
+	n_filters = 64
+	for k in range(depth):
+		for i in range(n_slices):
+				x2 = residual_block1D(x2,n_filters)
+		n_filters *= 2
+		if use_pooling and k!=(depth-1):
+			x2 = MaxPooling1D()(x2)
 
-# x2 = GlobalAveragePooling1D()(x2)
-# if dense_collection>0:
-# 	x2 = Dense(dense_collection)(x2)
-# if dropout_rate>0:
-# 	x2 = Dropout(dropout_rate)(x2)
+	x2 = GlobalAveragePooling1D()(x2)
+	if dense_collection>0:
+		x2 = Dense(dense_collection)(x2)
+	if dropout_rate>0:
+		x2 = Dropout(dropout_rate)(x2)
 
-# x2 = Dense(neurons_final,activation=final_activation,name=header)(x2)
-# model = Model(inputs, x2, name=header) 
+	x2 = Dense(neurons_final,activation=final_activation,name=header)(x2)
+	model = Model(inputs, x2, name=header) 
 
-# return model
-
-# if header=="classifier":
-# 	final_activation = "softmax"
-# 	neurons_final = n_classes
-# elif header=="regressor":
-# 	final_activation = "linear"
-# 	neurons_final = 1
-# else:
-# 	return None
-
-# inputs = Input(shape=(model_signal_length,n_channels,))
-# x2 = Conv1D(64, kernel_size=1,strides=1,padding='same')(inputs)
-
-# n_filters = 64
-# for k in range(depth):
-# 	for i in range(n_slices):
-# 			x2 = residual_block1D(x2,n_filters, kernel_size=8)
-# 	n_filters *= 2
-# 	if use_pooling and k!=(depth-1):
-# 		x2 = MaxPooling1D()(x2)
-
-# x2 = GlobalAveragePooling1D()(x2)
-# if dense_collection>0:
-# 	x2 = Dense(dense_collection)(x2)
-# if dropout_rate>0:
-# 	x2 = Dropout(dropout_rate)(x2)
-
-# x2 = Dense(neurons_final,activation=final_activation,name=header)(x2)
-# model = Model(inputs, x2, name=header)
-
-# return model
-
-# def MultiScaleResNetModel(n_channels, n_classes = 3, dropout_rate=0, dense_collection=0, use_pooling=True, depth=2,
-# 				 header="classifier", model_signal_length = 128):
-
-# 	if header=="classifier":
-# 		final_activation = "softmax"
-# 		neurons_final = n_classes
-# 	elif header=="regressor":
-# 		final_activation = "linear"
-# 		neurons_final = 1
-# 	else:
-# 		return None
-
-# 	inputs = Input(shape=(model_signal_length,n_channels,))
-# 	x = ZeroPadding1D(3)(inputs)
-# 	x = Conv1D(64, kernel_size=7, strides=2, padding="valid", use_bias=False)(x)
-# 	x = BatchNormalization()(x)
-# 	x = ZeroPadding1D(1)(x)
-# 	x_common = MaxPooling1D(pool_size=3, strides=2, padding='valid')(x)
-
-# 	# Block 1
-# 	x1 = residual_block1D(x_common, 64, kernel_size=7,connection='projection')
-# 	x1 = residual_block1D(x1, 128, kernel_size=7,connection='projection')
-# 	x1 = residual_block1D(x1, 256, kernel_size=7,connection='projection')
-# 	x1 = GlobalAveragePooling1D()(x1)
-
-# 	# Block 2
-# 	x2 = residual_block1D(x_common, 64, kernel_size=5,connection='projection')
-# 	x2 = residual_block1D(x2, 128, kernel_size=5,connection='projection')
-# 	x2 = residual_block1D(x2, 256, kernel_size=5,connection='projection')
-# 	x2 = GlobalAveragePooling1D()(x2)
-
-# 	# Block 3
-# 	x3 = residual_block1D(x_common, 64, kernel_size=3,connection='projection')
-# 	x3 = residual_block1D(x3, 128, kernel_size=3,connection='projection')
-# 	x3 = residual_block1D(x3, 256, kernel_size=3,connection='projection')
-# 	x3 = GlobalAveragePooling1D()(x3)
-
-# 	x_combined = Concatenate()([x1, x2, x3])
-# 	x_combined = Flatten()(x_combined)
-
-# 	if dense_collection>0:
-# 		x_combined = Dense(dense_collection)(x_combined)
-# 	if dropout_rate>0:
-# 		x_combined = Dropout(dropout_rate)(x_combined)
-
-# 	x_combined = Dense(neurons_final,activation=final_activation,name=header)(x_combined)
-# 	model = Model(inputs, x_combined, name=header) 
-
-# 	return model
+	return model
 
 
 def train_signal_model(config):

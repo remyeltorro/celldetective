c5ae132379a0165242b49800f6d822b6739e86ce
Rémy Torro <remy.torro@etu.univ-amu.fr>
2024-01-20 17:43:35 +0100

    fix

--- celldetective/io.py
+++ celldetective/io.py
@@ -41,7 +41,7 @@ def get_spatial_calibration(experiment):
 	
 	
 	config = get_config(experiment)
-	PxToUm = float(ConfigSectionMap(self.exp_config,"MovieSettings")["pxtoum"])
+	PxToUm = float(ConfigSectionMap(exp_config,"MovieSettings")["pxtoum"])
 	
 	return PxToUm
 

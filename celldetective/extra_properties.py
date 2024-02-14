69dea1c1fffc8f0843f0a0708f68c7194eedb4d0
Rémy Torro <remy.torro@etu.univ-amu.fr>
2024-02-14 10:22:38 +0100

    feat: std intensity

--- celldetective/extra_properties.py
+++ celldetective/extra_properties.py
@@ -1,4 +1,14 @@
+"""
+Add extra properties to directly to regionprops
+Functions must take regionmask as first argument and optionally intensity_image as second argument
+If intensity is in function name, it will be replaced by the name of the channel. These measurements are applied automatically to all channels
+
+"""
+
 import numpy as np
+
+
+# Percentiles
 
 def intensity_percentile_99(regionmask, intensity_image):
 	return np.nanpercentile(intensity_image[regionmask],99)
@@ -17,3 +27,9 @@ def intensity_percentile_50(regionmask, intensity_image):
 
 def intensity_percentile_25(regionmask, intensity_image):
 	return np.nanpercentile(intensity_image[regionmask],25)
+
+# STD
+
+def intensity_std(regionmask, intensity_image):
+	return np.nanstd(intensity_image[regionmask])
+

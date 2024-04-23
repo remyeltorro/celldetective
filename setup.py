a21a404cef86a1731c26be2e1c1db5ab0b2453a0
remy13127 <50580806+remy13127@users.noreply.github.com>
2024-04-23 16:25:03 +0200

    fix: pypi

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 import setuptools
 
 setup(name='celldetective',
-      version='1.0.2',
-      description='description',
-      url='http://github.com/remyeltorro/celldetective',
-      author='Rémy Torro',
-      author_email='remy.torro@inserm.fr',
-      license='GPL-3.0',
-      packages=setuptools.find_packages(),
-      zip_safe=False,
-      package_data={'celldetective': ['*','scripts/*','gui/*','models/*/*/*','models/*','models/*/*','icons/*','links/*','datasets/*', 'datasets/*/*']},
-      entry_points = {
-        'console_scripts': [
-          'celldetective = celldetective.__main__:main']
-     }
-      )
+			version='1.0.2',
+			description='description',
+			url='http://github.com/remyeltorro/celldetective',
+			author='Rémy Torro',
+			author_email='remy.torro@inserm.fr',
+			license='GPL-3.0',
+			packages=setuptools.find_packages(),
+			zip_safe=False,
+			package_data={'celldetective': ['*','scripts/*','gui/*','models/*/*/*','models/*','models/*/*','icons/*','links/*','datasets/*', 'datasets/*/*']},
+			entry_points = {
+				'console_scripts': [
+					'celldetective = celldetective.__main__:main']
+		 }
+			)
 

1bac72a1fdfff73a13131ce442da081e6362ea48
remy13127 <50580806+remy13127@users.noreply.github.com>
2024-04-23 16:26:56 +0200

    fix: pypi longdesc

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import setuptools
 setup(name='celldetective',
 			version='1.0.2',
 			description='description',
+			long_description=open('README.rst').read(),
+			long_description_content_type='text/markdown',
 			url='http://github.com/remyeltorro/celldetective',
 			author='Rémy Torro',
 			author_email='remy.torro@inserm.fr',

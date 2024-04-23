edc3a7635e31552d75d70eef54d1b5a69a6eef8e
remy13127 <50580806+remy13127@users.noreply.github.com>
2024-04-23 17:25:54 +0200

    fix: pypi with requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 from setuptools import setup
 import setuptools
+import pip
+
+links = []
+requires = []
+
+try:
+		requirements = pip.req.parse_requirements('requirements.txt')
+except:
+		# new versions of pip requires a session
+		requirements = pip.req.parse_requirements(
+				'requirements.txt', session=pip.download.PipSession())
+
+for item in requirements:
+		# we want to handle package names and also repo urls
+		if getattr(item, 'url', None):  # older pip has url
+				links.append(str(item.url))
+		if getattr(item, 'link', None): # newer pip has link
+				links.append(str(item.link))
+		if item.req:
+				requires.append(str(item.req))
 
 setup(name='celldetective',
 			version='1.0.2',
 			description='description',
 			long_description=open('README.rst',encoding="utf8").read(),
-      long_description_content_type='text/markdown',
+			long_description_content_type='text/markdown',
 			url='http://github.com/remyeltorro/celldetective',
 			author='Rémy Torro',
 			author_email='remy.torro@inserm.fr',
@@ -15,7 +35,9 @@ setup(name='celldetective',
 			package_data={'celldetective': ['*','scripts/*','gui/*','models/*/*/*','models/*','models/*/*','icons/*','links/*','datasets/*', 'datasets/*/*']},
 			entry_points = {
 				'console_scripts': [
-					'celldetective = celldetective.__main__:main']
-		 }
+					'celldetective = celldetective.__main__:main'],
+			},
+			install_requires = requires,
+			dependency_links = links
 			)
 

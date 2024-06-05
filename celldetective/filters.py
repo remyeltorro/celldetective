86c8f7ddff2a7f720f8fd03414a2de8d6805709a
Rémy Torro <50580806+remyeltorro@users.noreply.github.com>
2024-06-05 14:07:17 +0200

    refactor: more stability for filter with NaNs by changing mode

--- celldetective/filters.py
+++ celldetective/filters.py
@@ -35,8 +35,8 @@ def variance_filter(img, size):
 
 	size = int(size)
 	img = img.astype(float)
-	win_mean = snd.uniform_filter(img, (size,size))
-	win_sqr_mean = snd.uniform_filter(img**2, (size,size))
+	win_mean = snd.uniform_filter(img, (size,size), mode='wrap')
+	win_sqr_mean = snd.uniform_filter(img**2, (size,size), mode='wrap')
 	img = win_sqr_mean - win_mean**2
 
 	return img
@@ -45,8 +45,8 @@ def std_filter(img, size):
 
 	size = int(size)
 	img = img.astype(float)
-	win_mean = snd.uniform_filter(img, (size,size))
-	win_sqr_mean = snd.uniform_filter(img**2, (size, size))
+	win_mean = snd.uniform_filter(img, (size,size), mode='wrap')
+	win_sqr_mean = snd.uniform_filter(img**2, (size, size), mode='wrap')
 	win_sqr_mean[win_sqr_mean!=win_sqr_mean] = 0.
 	win_sqr_mean[win_sqr_mean<=0.] = 0. # add this to prevent sqrt from breaking
 	img = np.sqrt(win_sqr_mean - win_mean**2)

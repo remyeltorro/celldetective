4910911c155719226cfedcd0da8bbb973b471e34
Rémy Torro <remy.torro@etu.univ-amu.fr>
2024-09-25 17:43:26 +0200

    fix: documentation breaking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,20 @@
 import sphinx_rtd_theme
 import sys
 import os
-import celldetective
+import re
 
 #sys.path.append('/home/limozin/Documents/GitHub/rtd-tutorial')
 sys.path.insert(0, os.path.abspath('./../../'))
 sys.path.insert(0, os.path.abspath('./../../examples/'))
+
+VERSIONFILE = os.path.abspath('./../../celldetective/_version.py')
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+	verstr = mo.group(1)
+else:
+	raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 
 # Configuration file for the Sphinx documentation builder.
@@ -23,21 +32,21 @@ version = vers #'1.2.2.post2'
 # -- General configuration
 
 extensions = [
-    'nbsphinx',
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.viewcode",
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
-    'nbsphinx_link',
+	'nbsphinx',
+	"sphinx.ext.autodoc",
+	"sphinx.ext.intersphinx",
+	"sphinx.ext.mathjax",
+	"sphinx.ext.viewcode",
+	'sphinx.ext.napoleon',
+	'sphinx.ext.autosummary',
+	'nbsphinx_link',
 ]
 
 autoapi_dirs = ['celldetective']
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+	'python': ('https://docs.python.org/3/', None),
+	'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
